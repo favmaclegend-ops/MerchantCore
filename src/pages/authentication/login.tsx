415c019e5637950ef5e0125a172755fd0b7c6b24
ElktrumElk <festinacole373@gmail.com>
2026-05-04 21:35:10 +0000

    update 0.1

--- src/pages/authentication/login.tsx
+++ src/pages/authentication/login.tsx
@@ -15,15 +15,19 @@ export default function LoginPage() {
                 <form className="form">
 
                     <div>
-                        <input id="user-input" className="inp" placeholder="Username" required />
+                        <input id="user-input" className="inp" placeholder="Username" type="text" required />
                     </div>
 
                     <div>
-                        <input id="user-email" className="inp" placeholder="Email Address" required />
+                        <input id="user-email" className="inp" placeholder="Email Address" required type="email" />
                     </div>
 
                     <div>
                         <input id="user-password" className="inp" placeholder="Password" type="password" required />
+                    </div>
+
+                    <div>
+                        <button id="sub-btn" className="sub-btn-auth" type="submit">Submit</button>
                     </div>
 
                 </form>
