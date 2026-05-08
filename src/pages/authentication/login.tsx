066a6af1e34558fd24262807952bff87b3b706d4
ElktrumElk <festinacole373@gmail.com>
2026-05-08 12:54:03 +0000

    Login UI update

--- src/pages/authentication/login.tsx
+++ src/pages/authentication/login.tsx
@@ -1,50 +1,32 @@
-import { logData } from "@/account/data/login_data";
 import { useRef, useState } from "react"
 import { useNavigate } from "react-router-dom";
 import AlertDialog from "../../components/layout/alert_dialog";
+import { login } from "@/account/authentication/auth";
 
 
 
 export default function LoginPage() {
 
-    const username = useRef<HTMLInputElement>(null);
+   
     const email = useRef<HTMLInputElement>(null);
     const password = useRef<HTMLInputElement>(null);
     const form = useRef<HTMLFormElement>(null);
     const [alertInfo, setAlert] = useState({message: '', type: ''});
     const [isAlert, setIsAlert] = useState('none');
+    const [isLoading, setIsLoading] = useState(false);
 
-    const { userDatabase } = logData();
-    const navigate = useNavigate();
+   // const navigate = useNavigate();
 
+    login
     
-    const handleAuthentication = (e: React.SubmitEvent<HTMLFormElement>) => {
-
+    const handleAuthentication = async (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault();
-        const key = username.current?.value;
+        
+        setIsLoading(true);
+        const data = await login({email: email.current?.value, password: password.current?.value});
+        setIsLoading(false);
+        console.log(data);
 
-        if (userDatabase[`${key}`]) {
-
-            if (email.current?.value === userDatabase[`${key}`].email &&
-                password.current?.value === userDatabase[`${key}`].password
-            ) {
-                setAlert({message: 'Login SuccessFul', type: 'success'});
-                setIsAlert('flex');
-                
-                setTimeout(() => {
-                    navigate('/home/dashboard', { replace: true });
-                }, 1500);
-            }
-            else {
-                setIsAlert('flex');
-                setAlert({message: 'Invalid Credential', type: 'invalid'});
-            }
-        }
-
-        else {
-            setIsAlert('flex');
-            setAlert({message: 'Invalid Credential', type: 'invalid'});
-        }
     }
     
     return (
@@ -61,10 +43,6 @@ export default function LoginPage() {
                 <form ref={form} className="form" onSubmit={(e) => handleAuthentication(e)}>
 
                     <div>
-                        <input ref={username} id="user-input" className="inp" placeholder="Username" type="text" required />
-                    </div>
-
-                    <div>
                         <input ref={email} id="user-email" className="inp" placeholder="Email Address" required type="email" />
                     </div>
 
@@ -73,7 +51,7 @@ export default function LoginPage() {
                     </div>
 
                     <div>
-                        <button id="sub-btn" className="sub-btn-auth" type="submit">Submit</button>
+                        <button id="sub-btn" className={`sub-btn-auth ${isLoading ? 'loading-bg-2' : ''}`} type="submit" disabled={isLoading}>{isLoading ? 'Validating...' : 'Submit'}</button>
                     </div>
 
                 </form>
