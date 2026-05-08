046d05d5440f14bf44fd2bd3bdaf221fba0005dc
ElktrumElk <festinacole373@gmail.com>
2026-05-08 09:29:58 +0000

    c update 0.0.1

--- src/components/layout/alert_dialog.tsx
+++ src/components/layout/alert_dialog.tsx
@@ -5,13 +5,27 @@ interface AlertProps {
     }
 }
 
+interface alertIc {
+    success: string,
+    invalid: string,
+    error: string,
+    warning: string
+}
+
 
 export default function AlertDialog({alert}: AlertProps) {
+
+    const alertType: alertIc = {
+        success: 'https://img.icons8.com/?size=100&id=11695&format=png&color=000000',
+        invalid: 'https://img.icons8.com/?size=100&id=3062&format=png&color=000000',
+        error: 'https://img.icons8.com/?size=100&id=360&format=png&color=000000',
+        warning: 'https://img.icons8.com/?size=100&id=24549&format=png&color=000000',
+    }
 
     return (
         <>
         <div className="alert_cnt-2">
-            <img src={alert.type}/>
+            <img src={alertType[`${alert.type}`]}/>
             <p>{alert.message}</p>
         </div>
         </>
